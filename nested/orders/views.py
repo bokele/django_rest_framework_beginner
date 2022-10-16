aab9f1ac2d7c049d1f7d50b1093f124f0ffe23e8
Bokele Wakiza Franck <bwakiza@gmail.com>
2022-10-16 11:16:04 +0200

    first commit

--- nested/orders/views.py
+++ nested/orders/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-from nested.orders.serializer import CustomerSerializer
 from .models import Customer, Order
-
-from orders.serializer import OrderSerializer, CustomSerializer
+from orders.serializer import OrderSerializer, CustomerSerializer
 from rest_framework import generics
 
 # Create your views here.
